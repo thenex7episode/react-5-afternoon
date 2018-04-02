4cb46c8dd0ed270043f11ca09cd667a6250207fb
William <lambermontwilliam@gmail.com>
2018-04-02 15:52:42 -0700

    1-4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import {HashRouter as Router} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store = {store}>
+    <Router>
+        <App />
+    </Router>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
 
